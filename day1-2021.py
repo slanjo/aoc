1458c416014459e2991d54061f7a06c499f5816a
slanjo <slaanjoo@gmail.com>
2021-12-01 12:34:35 +0000

    2021-Day1

--- day1-2021.py
+++ day1-2021.py
@@ -1,4 +1,5 @@
 '''Advent of code 2021 Day 1'''
+
 from os import terminal_size
 
 
@@ -15,37 +16,22 @@ for i in txt_input:
     num_input.append(int(i))
 
 
-for i in range(1, len(num_input) - 1):
-    print(f"element {i} is {num_input[i]}") 
+for i in range(1, len(num_input)):
     if num_input[i]  > num_input[i - 1]:
         count_increased += 1
-        print(f"counter = {count_increased}==== i is {i}")
-
-if num_input[len(num_input) - 1] >  num_input[len(num_input) - 2]:
-    count_increased += 1
 
 print(count_increased)
 
 ### ||| part two |||
 
-#print(txt_input)
-#print(num_input)
-
 
 suma = []
-for i in range(2, len(num_input) - 1):
+for i in range(2, len(num_input)):
     suma.append(num_input[i] + num_input[i - 1] + num_input[i - 2])
-suma.append(num_input[-1] + num_input[-2] + num_input[-3] )
-print(f"-------------------- last element{suma[len(suma) -1]}")
-print(suma)
+
 count_increased_suma = 0
-for x in range(1, len(suma) - 1):
-    print(f"element {x} is {suma[x]}") 
+for x in range(1, len(suma)):
     if suma[x]  > suma[x - 1]:
         count_increased_suma += 1
-        print(f"counter = {count_increased_suma}==== i is {x}")
-
-if suma[len(suma) - 1] >  suma[len(suma) - 2]:
-    count_increased_suma += 1
 
 print(count_increased_suma)
