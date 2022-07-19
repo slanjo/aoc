dd0a5f4f5299169cee41e2eeedda708da315c579
sanjin zoric <s.zoric@gmail.com>
2022-07-19 22:35:17 +0800

    aocDay5

--- day5/day5-2021.py
+++ day5/day5-2021.py
@@ -1,17 +1,21 @@
-#import collections
+DELIMITER = "->"
+import collections
 from collections import namedtuple
 
 if __name__ == '__main__':
     lines = []
-    with open("/home/slanjo/Programming/aoc/day5/input.txt", "r") as data_in:
+    with open("/home/slanjo/Programming/aoc/day5/input.py", "r") as data_in:
         line = data_in.readlines()
         for string in line:
-            lines.append(string.strip('\n'))
+            lines.append(string.strip('\n').split(' -> '))
 
     OneLine = collections.namedtuple('OneLine', 'tocka1, junk, tocka2')
-    for tocka in map(OneLine._make, "input.txt"):
-        print(tocka.tocka1, tocka.tocka2)
     print(line)
     print(len(line))
     print(lines)
     print(len(lines))
+
+    for string in lines:
+        string[0].split(',')
+        string[1].split(',')
+
