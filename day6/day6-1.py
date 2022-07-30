fe2c81a97cc6b237d2576dc83388dfb5dc5a01d5
sanjin zoric <s.zoric@gmail.com>
2022-07-30 08:38:19 +0800

    day6-1

---
+++ day6/day6-1.py
@@ -0,0 +1,15 @@
+
+"""
+    every time we enter the loop we:
+        check if lanternfish internaltimer==0:
+            set that lanterfish internaltimer to 6
+            create a new lantern fish and set its internaltimer to 8 -- append 8 to the end of list
+
+"""
+from ulaz import test_input, input, sat1_input
+
+
+if __name__ == '__main__':
+    print(test_input)
+    print(f"Length of input: {len(input)}")
+
