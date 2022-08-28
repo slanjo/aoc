3760ce57f0c5f95cd5588444048bc2d03331e3ef
sanjin zoric <s.zoric@gmail.com>
2022-08-28 20:39:00 +0800

    Day8

--- day8/day8-2021-1-1.py
+++ day8/day8-2021-1-1.py
@@ -1,11 +1,24 @@
-#!/usr/bin/python3.10
+#Jonatha Paulson https://www.youtube.com/watch?v=DhQPrF-LBoE
+#!/usr/bin/python3
 import sys
 from collections import defaultdict, Counter
 
 if __name__ == '__main__':
-
+    ans = 0
     f = sys.argv[1] if len(sys.argv) > 1 else '8.in'
     for line in open(f, "r"):
         before, after = line.split("|")
-        
-        print(after)
+        before = before.split()
+        after =  after.split()
+        by_len = defaultdict(list)
+        for w in before:
+            by_len[len(w)].append(w)
+
+        print(f"by_len = {by_len}")
+        for w in after:
+            if len(by_len[len(w)]) == 1:
+                print(by_len[len(w)])
+                ans += 1
+#       print(by_len)
+#    print(by_len)
+    print(ans)
